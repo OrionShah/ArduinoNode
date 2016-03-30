3e70afc2e1dcd11e636d0d0167a80f673ef88b1d
Andrey Shahov <andrey.shahov@webpp.ru>
2016-03-30 17:11:31 +0300

    Поправил код

--- scetch/rx/rx.ino
+++ scetch/rx/rx.ino
@@ -2,26 +2,26 @@
 // Tx на пине D12
 #include <VirtualWire.h>
 void setup() {
-  Serial.begin(9600);
-  vw_set_ptt_inverted(true); // необходимо для модуля DR3100
-  vw_set_rx_pin(12);
-  vw_setup(200); // бит в секунду
-  pinMode(13, OUTPUT);
-  vw_rx_start(); // запуск фазовой автоподстройки частоты (ФАПЧ)
+    Serial.begin(9600);
+    vw_set_ptt_inverted(true); // необходимо для модуля DR3100
+    vw_set_rx_pin(12);
+    vw_setup(200); // бит в секунду
+    pinMode(13, OUTPUT);
+    vw_rx_start(); // запуск фазовой автоподстройки частоты (ФАПЧ)
 }
 
 void loop() {
-  uint8_t buf[VW_MAX_MESSAGE_LEN];
-  uint8_t buflen = VW_MAX_MESSAGE_LEN;
-  if (vw_get_message(buf, &buflen)) { // Не блокируется
-    if(buf[0]=='1'){
-      Serial.println("Get something 1");
-      digitalWrite(13,1);
+    uint8_t buf[VW_MAX_MESSAGE_LEN];
+    uint8_t buflen = VW_MAX_MESSAGE_LEN;
+    if (vw_get_message(buf, &buflen)) { // Не блокируется
+        if (buf[0]=='1') {
+            Serial.println("Get something 1");
+            digitalWrite(13,1);
+        }
+
+        if (buf[0]=='0') {
+            Serial.println("Get something 0");
+            digitalWrite(13,0);
+        }
     }
-    
-    if(buf[0]=='0'){
-      Serial.println("Get something 0");
-      digitalWrite(13,0);
-    }
-  }
 }
--- scetch/tx/tx.ino
+++ scetch/tx/tx.ino
@@ -4,26 +4,26 @@
 char *controller;
 char str;
 void setup() {
-//  Serial.begin(9600);
-  pinMode(13,OUTPUT);
-  vw_set_ptt_inverted(true);
-  vw_set_tx_pin(12);
-  vw_setup(200);// скорость передачи данных в Kbps
+    // Serial.begin(9600);
+    pinMode(13,OUTPUT);
+    vw_set_ptt_inverted(true);
+    vw_set_tx_pin(12);
+    vw_setup(200);// скорость передачи данных в Kbps
 }
 
 void loop(){
-  str='1';
-  controller=&str;
-  vw_send((uint8_t *)controller, strlen(controller));
-  vw_wait_tx(); // ждем, пока отправится все сообщение
-  digitalWrite(13,1);
-//  Serial.println("send 1");
-  delay(1000);
-  str='0';
-  controller=&str;
-  vw_send((uint8_t *)controller, strlen(controller));
-  vw_wait_tx(); // ждем, пока отправится все сообщение
-  digitalWrite(13,0);
-//  Serial.println("send 0");
-  delay(1000);
+    str='1';
+    controller=&str;
+    vw_send((uint8_t *)controller, strlen(controller));
+    vw_wait_tx(); // ждем, пока отправится все сообщение
+    digitalWrite(13,1);
+    // Serial.println("send 1");
+    delay(250);
+    str='0';
+    controller=&str;
+    vw_send((uint8_t *)controller, strlen(controller));
+    vw_wait_tx(); // ждем, пока отправится все сообщение
+    digitalWrite(13,0);
+    // Serial.println("send 0");
+    delay(250);
 }
